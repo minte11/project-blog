40bf691c4dd87a6dc7537904d3ca719e3ade99ec
egor.kozyrev <egor.kozyrev@newfold.com>
2025-04-03 13:38:50 +0300

    finished wiring CircularColorsDemo

--- src/components/CircularColorsDemo/CircularColorsDemo.js
+++ src/components/CircularColorsDemo/CircularColorsDemo.js
@@ -1,86 +1,113 @@
+'use client';
 import React from 'react';
 import clsx from 'clsx';
 import {
-  Play,
-  Pause,
-  RotateCcw,
+	Play,
+	Pause,
+	RotateCcw,
 } from 'react-feather';
-
+import {motion} from 'framer-motion';
 import Card from '@/components/Card';
 import VisuallyHidden from '@/components/VisuallyHidden';
 
 import styles from './CircularColorsDemo.module.css';
 
 const COLORS = [
-  { label: 'red', value: 'hsl(348deg 100% 60%)' },
-  { label: 'yellow', value: 'hsl(50deg 100% 55%)' },
-  { label: 'blue', value: 'hsl(235deg 100% 65%)' },
+	{label: 'red', value: 'hsl(348deg 100% 60%)'},
+	{label: 'yellow', value: 'hsl(50deg 100% 55%)'},
+	{label: 'blue', value: 'hsl(235deg 100% 65%)'},
 ];
 
-function CircularColorsDemo() {
-  // TODO: This value should increase by 1 every second:
-  const timeElapsed = 0;
+function CircularColorsDemo () {
+	const [timeElapsed, setTimeElapsed] = React.useState(0);
+	const [isPlaying, setIsPlaying] = React.useState(false);
+	const intervalRef = React.useRef(null);
+	
+	const handlePlayPause = () => setIsPlaying(!isPlaying);
+	
+	const handleReset = () => {
+		setTimeElapsed(0);
+		setIsPlaying(false);
+	};
+	
+	React.useEffect(() => {
+		if (!isPlaying) {
+			return clearInterval(intervalRef.current);
+		}
+		
+		const interval = setInterval(() => {
+			setTimeElapsed((previousVal) => previousVal + 1);
+		}, 1000)
+		intervalRef.current = interval;
+		return () => clearInterval(interval);
+	}, [isPlaying]);
 
-  // TODO: This value should cycle through the colors in the
-  // COLORS array:
-  const selectedColor = COLORS[0];
-
-  return (
-    <Card as="section" className={styles.wrapper}>
-      <ul className={styles.colorsWrapper}>
-        {COLORS.map((color, index) => {
-          const isSelected =
-            color.value === selectedColor.value;
-
-          return (
-            <li
-              className={styles.color}
-              key={index}
-            >
-              {isSelected && (
-                <div
-                  className={
-                    styles.selectedColorOutline
-                  }
-                />
-              )}
-              <div
-                className={clsx(
-                  styles.colorBox,
-                  isSelected &&
-                    styles.selectedColorBox
-                )}
-                style={{
-                  backgroundColor: color.value,
-                }}
-              >
-                <VisuallyHidden>
-                  {color.label}
-                </VisuallyHidden>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-
-      <div className={styles.timeWrapper}>
-        <dl className={styles.timeDisplay}>
-          <dt>Time Elapsed</dt>
-          <dd>{timeElapsed}</dd>
-        </dl>
-        <div className={styles.actions}>
-          <button>
-            <Play />
-            <VisuallyHidden>Play</VisuallyHidden>
-          </button>
-          <button>
-            <RotateCcw />
-            <VisuallyHidden>Reset</VisuallyHidden>
-          </button>
-        </div>
-      </div>
-    </Card>
-  );
+	const selectedColor = COLORS[timeElapsed % COLORS.length];
+	
+	return (
+		<Card as="section" className={styles.wrapper}>
+			<ul className={styles.colorsWrapper}>
+				{COLORS.map((color, index) => {
+					const isSelected =
+						color.value === selectedColor.value;
+					
+					return (
+						<motion.li
+							className={styles.color}
+							key={index}
+						>
+							{isSelected && (
+								<motion.div
+									layoutId="underline"
+									className={
+										styles.selectedColorOutline
+									}
+								/>
+							)}
+							<div
+								className={clsx(
+									styles.colorBox,
+									isSelected &&
+									styles.selectedColorBox
+								)}
+								style={{
+									backgroundColor: color.value,
+								}}
+							>
+								<VisuallyHidden>
+									{color.label}
+								</VisuallyHidden>
+							</div>
+						</motion.li>
+					);
+				})}
+			</ul>
+			
+			<div className={styles.timeWrapper}>
+				<dl className={styles.timeDisplay}>
+					<dt>Time Elapsed</dt>
+					<dd>{timeElapsed}</dd>
+				</dl>
+				<div className={styles.actions}>
+					<button onClick={handlePlayPause}>
+						{isPlaying ?
+							<>
+								<Pause/>
+								<VisuallyHidden>Pause</VisuallyHidden>
+							</> : <>
+								<Play/>
+								<VisuallyHidden>Play</VisuallyHidden>
+							</>
+						}
+					</button>
+					<button onClick={handleReset}>
+						<RotateCcw/>
+						<VisuallyHidden>Reset</VisuallyHidden>
+					</button>
+				</div>
+			</div>
+		</Card>
+	);
 }
 
 export default CircularColorsDemo;
--- src/helpers/mdx-components-mapping.js
+++ src/helpers/mdx-components-mapping.js
@@ -1,9 +1,11 @@
 import CodeSnippet from "@/components/CodeSnippet";
 import DivisionGroupsDemo from "@/components/DivisionGroupsDemo";
+import CircularColorsDemo from "@/components/CircularColorsDemo";
 
 const COMPONENTS_MAPPING = {
 	pre: CodeSnippet,
 	DivisionGroupsDemo,
+	CircularColorsDemo,
 }
 
 export default COMPONENTS_MAPPING;
